eae6956afeed9ab8945460aa2366f4be59756c56
Francisco Cabelo <fangelo@cpqd.com.br>
2019-09-01 23:43:22 -0300

    Fixing lint part 2

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Button } from '@material-ui/core';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -7,16 +7,16 @@ import { DojotButton, CleanButton, CustomButton } from '../Components/Button';
 
 function Checkboxes() {
     return (
-        <Fragment>
+        <>
             <Checkbox defaultChecked />
             <Checkbox defaultChecked color="primary" />
             <Checkbox defaultChecked color="secondary" />
-        </Fragment>
+        </>
     );
 }
 
 const Main = () => (
-    <Fragment>
+    <>
         <Grid container justify="center">
             <Checkboxes />
             <CleanButton a="x">CleanButton</CleanButton>
@@ -25,7 +25,7 @@ const Main = () => (
             <Button> Material Button</Button>
             <Loader />
         </Grid>
-    </Fragment>
+    </>
 );
 
 export default Main;
