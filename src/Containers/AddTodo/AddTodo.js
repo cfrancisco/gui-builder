603b105894e420ee07f509e9f484ba1cd7dd6cce
nunesjoab <nunesjoab@gmail.com>
2019-09-17 11:06:03 -0300

    Changed to a React Component implementation

--- src/Containers/AddTodo/AddTodo.js
+++ src/Containers/AddTodo/AddTodo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {
     TextField,
@@ -21,76 +21,96 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-let priority;
+class AddTodo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            priority: '',
+            input: '',
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleInput = this.handleInput.bind(this);
+    }
 
-const AddTodo = ({ dispatch }) => {
-
+    /*
     const classes = useStyles();
     let input;
     
     //used to set default value to radio buttons
     const [value, setValue] = React.useState('low');
+    */
 
-    function handleChange(event) {
-        setValue(event.target.value);
-        priority = event.target.value;
+    handleSubmit(event) {
+        if (!this.state.input.trim()) {
+            return;
+        } else {
+            this.setState({input: event.target.value});
+        }
+        this.props.dispatch(addTodo(this.state.input, this.state.priority));
+        this.setState({input: ''});
+        event.preventDefault();
+    }
+
+    handleInput(event) {
+        this.setState({input: event.target.value});
     }
     
-    return (
-        <form
-            onSubmit={(e) => {
-                e.preventDefault();
-                if (!input.value.trim()) {
-                    return;
-                }
-                dispatch(addTodo(input.value, priority));
-                input.value = '';
-            }}
-        >
-            <Grid
-                container
-                spacing={2}
+    handleChange(event) {
+        this.setState({priority: event.target.value});
+    }
+    
+    render() {
+        return (
+            <form
+                onSubmit={this.handleSubmit}
             >
-                <Grid item xs={3}>
-                    <TextField
-                        id="todo-text"
-                        label="Todo"
-                        inputRef={node => (input = node)}
-                        margin="normal"
-                    />
-                </Grid>
+                <Grid
+                    container
+                    spacing={2}
+                >
+                    <Grid item xs={3}>
+                        <TextField
+                            id="todo-text"
+                            label="Todo"
+                            value={this.state.input}
+                            onChange={this.handleInput}
+                            margin="normal"
+                        />
+                    </Grid>
 
-                <Grid item xs={3}>
-                    <FormControl component="fieldset" className={classes.formControl}>
-                        <FormLabel component="legend">Prioridade</FormLabel>
-                        <RadioGroup aria-label="priority" name="prioriry" value={value} onChange={handleChange}>
-                        <FormControlLabel
-                            value="low"
-                            control={<Radio color="primary" />}
-                            label="Baixa"
-                            labelPlacement="end"
-                        />
-                        <FormControlLabel
-                            value="medium"
-                            control={<Radio color="primary" />}
-                            label="Média"
-                            labelPlacement="end"
-                        />
-                        <FormControlLabel
-                            value="high"
-                            control={<Radio color="primary" />}
-                            label="Alta"
-                            labelPlacement="end"
-                        />
-                        </RadioGroup>
-                    </FormControl>
+                    <Grid item xs={3}>
+                        <FormControl component="fieldset">
+                            <FormLabel component="legend">Prioridade</FormLabel>
+                            <RadioGroup aria-label="priority" name="prioriry" value={this.state.priority} onChange={this.handleChange}>
+                            <FormControlLabel
+                                value="low"
+                                control={<Radio color="primary" />}
+                                label="Baixa"
+                                labelPlacement="end"
+                            />
+                            <FormControlLabel
+                                value="medium"
+                                control={<Radio color="primary" />}
+                                label="Média"
+                                labelPlacement="end"
+                            />
+                            <FormControlLabel
+                                value="high"
+                                control={<Radio color="primary" />}
+                                label="Alta"
+                                labelPlacement="end"
+                            />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <Button type="submit" size="small">Add Todo</Button>
+                    </Grid>
                 </Grid>
-                <Grid item xs={3}>
-                    <Button type="submit" size="small">Add Todo</Button>
-                </Grid>
-            </Grid>
-        </form>
-    );
-};
+            </form>
+        );
+    }
+}
 
 export default connect()(AddTodo);
