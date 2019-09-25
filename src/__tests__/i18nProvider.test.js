5c3454fc8de6c4aa5150df85965a48d2b2884f60
nunesjoab <nunesjoab@gmail.com>
2019-09-25 14:44:46 -0300

    Test of locale files and fix of pt-BR file to match with en-US file

---
+++ src/__tests__/i18nProvider.test.js
@@ -0,0 +1,27 @@
+// import I18nProvider from '../Components/i18nProvider/i18nProvider';
+import ptBR from '../Components/i18nProvider/locales/pt-BR.json';
+import enUS from '../Components/i18nProvider/locales/en-US.json';
+
+function getDeepKeys(obj) {
+    let keys = [];
+    for (const key of Object.keys(obj)) {
+        keys.push(key);
+        if (typeof obj[key] === 'object') {
+            const subkeys = getDeepKeys(obj[key]);
+            keys = keys.concat(subkeys.map((subkey) => `${key}.${subkey}`));
+        }
+    }
+    return keys;
+}
+
+describe('Testing functions of internacionalization component', () => {
+    beforeEach(() => jest.resetModules());
+
+    test('Both locale files must have the same keys', () => {
+
+        const ptKeys = getDeepKeys(ptBR);
+        const enKeys = getDeepKeys(enUS);
+
+        expect(ptKeys).toEqual(enKeys);
+    });
+});
