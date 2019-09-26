fe60a5158f20931ab1f47249fca971608639ed00
Francisco Cabelo <fangelo@cpqd.com.br>
2019-09-26 13:29:14 -0300

    Fixing Jest tests

--- src/Components/Avatar/utils.js
+++ src/Components/Avatar/utils.js
@@ -6,13 +6,17 @@ function getBase64(img, callback) {
     reader.readAsDataURL(img);
 }
 
+function isLowerThan2m(size) {
+    return size / 1024 / 1024 < 2;
+}
+
 function beforeUpload(file) {
     const { type, size } = file;
     const isJpgOrPng = type === 'image/jpeg' || type === 'image/png';
     if (!isJpgOrPng) {
         message.error('You can only upload JPG/PNG file!');
     }
-    const isLt2M = size / 1024 / 1024 < 2;
+    const isLt2M = isLowerThan2m(size);
     if (!isLt2M) {
         message.error('Image must smaller than 2MB!');
     }
---
+++ src/Components/Button/__tests__/Button.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+import { CustomButton } from '../index';
+
+configure({ adapter: new Adapter() });
+
+describe('CustomButton', () => {
+    /**
+     * Checks whether the CustomButton renders correctly without any props
+     */
+    it('should render correctly without any props', () => {
+        const wrapper = shallow(<CustomButton />);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    /**
+     * Checks the type props
+     */
+    it('check type props', () => {
+        const props = {
+            type: 'submit',
+        };
+        const wrapper = shallow(<CustomButton {...props} />).find('customButton');
+        expect(wrapper.props().type).toEqual('submit');
+    });
+
+    /**
+     * Checks the label props
+     */
+    it('check label props', () => {
+        const wrapper = shallow(<CustomButton>My custom label</CustomButton>);
+
+        expect(wrapper.props().children).toEqual('My custom label');
+    });
+
+    /**
+     * Checks the disabled props
+     */
+    it('check disabled props', () => {
+        const wrapper = shallow(<CustomButton disabled />);
+
+        expect(wrapper.props().disabled).toBeTruthy();
+    });
+
+    /**
+     * Checks the onClick props
+     */
+    it('check onClick props', () => {
+        const onClickFunction = jest.fn();
+        const wrapper = shallow(<CustomButton onClick={onClickFunction} />);
+
+        wrapper.find('customButton').simulate('click');
+
+        expect(onClickFunction).toHaveBeenCalled();
+    });
+});
