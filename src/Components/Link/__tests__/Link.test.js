eccca32bdca1d7fb4d970a60237fa6be1070b10b
Francisco Cabelo <fangelo@cpqd.com.br>
2019-09-26 14:28:40 -0300

    Updated Link test location

--- src/Components/Link/__tests__/Link.test.js
+++ src/Components/Link/__tests__/Link.test.js
@@ -3,9 +3,16 @@ import renderer from 'react-test-renderer';
 
 import Link from '../Link';
 
+it('Link should render correctly', () => {
+    const tree = renderer
+        .create(<Link />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
 it('Link with href attribute should render correctly', () => {
     const tree = renderer
-        .create(<Link href="whatever" />)
+        .create(<Link href="www.cpqd.com.br" />)
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
