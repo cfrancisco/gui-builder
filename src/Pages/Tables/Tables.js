d9a6b6da6bb21c4294e8eabc89c992564fabfaaa
Francisco Cabelo <fangelo@cpqd.com.br>
2019-10-03 17:38:42 -0300

    Adding Material Table

--- src/Pages/Tables/Tables.js
+++ src/Pages/Tables/Tables.js
@@ -1,31 +1,91 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import MaterialTable from 'material-table';
 import SimpleTable from '../../Components/Table/SimpleTable';
 import styles from './_styles';
+import tableIcons from '../../Components/MaterialTable/icons';
 
 const useStyles = makeStyles(styles);
 
 const Tables = () => {
     const classes = useStyles();
-    const header = [
+    const simpleHeader = [
         'Dessert (g)', 'Calories (g)', 'Fat (g)', 'Carbs (g)', 'Protein (g)',
     ];
-    const data = [
+    const simpleData = [
         ['Frozen yoghurt', 159, 6.0, 24, 4],
         ['Ice cream sandwich', 237, 9.0, 2, 37],
         ['Eclair', 262, 16.0, 24, 6.0],
         ['Cupcake', 305, 3.7, 67, 4.3],
         ['Gingerbread', 356, 16.0, 49, 3.9],
     ];
+    const [rawData, setRawData] = useState([]);
+
+    useEffect(() => {
+        setRawData([
+            {
+                name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63,
+            },
+            {
+                name: 'Zerya Betül', surname: 'Baran', birthYear: 2017, birthCity: 34,
+            },
+        ]);
+    }, []);
+
+    const columns = [
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname', initialEditValue: 'initial edit value' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+            title: 'Birth Place',
+            field: 'birthCity',
+            lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+    ];
+
+    const onRowAddMethod = (newData) => new Promise((resolve, reject) => {
+        const auxData = [...rawData];
+        auxData.push(newData);
+        setRawData(auxData);
+        resolve();
+    });
+
+    const onRowUpdateMethod = (newData, oldData) => new Promise((resolve, reject) => {
+        const auxData = [...rawData];
+        const index = auxData.indexOf(oldData);
+        auxData[index] = newData;
+        setRawData(auxData);
+        resolve();
+    });
+
+
+    const onRowDeleteMethod = (oldData) => new Promise((resolve, reject) => {
+        const auxData = [...rawData];
+        const index = auxData.indexOf(oldData);
+        auxData.splice(index, 1);
+        setRawData(auxData);
+        resolve();
+    });
+
     return (
         <div className={classes.root}>
             <Grid spacing={10} alignItems="center" justify="center" container>
                 <Grid item xs={12} md={12}>
-                    <SimpleTable header={header} data={data} />
+                    <SimpleTable header={simpleHeader} data={simpleData} />
                 </Grid>
                 <Grid item xs={12} md={12}>
-                    Complex Table
+                    <MaterialTable
+                        icons={tableIcons}
+                        title="Editable Table"
+                        columns={columns}
+                        data={rawData}
+                        editable={{
+                            onRowAdd: onRowAddMethod,
+                            onRowUpdate: onRowUpdateMethod,
+                            onRowDelete: onRowDeleteMethod,
+                        }}
+                    />
                 </Grid>
             </Grid>
         </div>
