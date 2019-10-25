04bb6b1685a5ad505f4a6af6d3ed619b58b5acc5
nunesjoab <nunesjoab@gmail.com>
2019-10-25 08:49:23 -0300

    Remove unused object

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -60,13 +60,6 @@ const lineChartDataset = [
         ],
     },
 ];
-const sampleData = [
-    ['Frozen yoghurt', 159, 6.0, 24, 4],
-    ['Ice cream sandwich', 237, 9.0, 2, 37],
-    ['Eclair', 262, 16.0, 24, 6.0],
-    ['Cupcake', 305, 3.7, 67, 4.3],
-    ['Gingerbread', 356, 16.0, 49, 3.9],
-];
 
 const barChartDataset = [
     {
