cf351541cb5a6c67dfd671041d63b1b79d66c50c
nunesjoab <nunesjoab@gmail.com>
2019-11-07 15:26:08 -0300

    Using material variables to set padding in shorthand form

--- src/Components/Table/_styles.js
+++ src/Components/Table/_styles.js
@@ -2,6 +2,7 @@ const styles = (theme) => ({
     root: {
         width: '100%',
         marginTop: theme.spacing(3),
+        padding: theme.spacing(0, 2),
         overflowX: 'auto',
     },
 });
